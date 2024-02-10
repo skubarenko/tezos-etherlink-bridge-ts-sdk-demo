f9102bb91b14c0ff3efb2ea576d763c3052a2822
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2024-02-10 13:43:23 +0300

    Add the @taquito packages Add a patch for the @airgap/beacon-ui package Update the next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+
+  /**
+   * 
+   * @param {import('webpack').Configuration} config 
+   * @param {Parameters<import('next').NextConfig['webpack']>[1]} context 
+   * @returns 
+   */
+  webpack: (config, context) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new context.webpack.IgnorePlugin({
+        resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+      })
+    );
+
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = config.resolve.fallback || {};
+    config.resolve.fallback.fs = false;
+
+    return config;
+  },
+};
 
 export default nextConfig;
